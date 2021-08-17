b1306144578b10e462685959f4a76157313e793a
Diego Morales <dmorales@stone.com.br>
2021-08-17 18:29:57 -0300

    Refactoring to reduce duplication of code.
    Also improved usability of the UserStore class by adding docstrings, more type hints.

--- src/personapi/api.py
+++ src/personapi/api.py
@@ -5,7 +5,7 @@ import uvicorn
 from typing import List
 from fastapi import FastAPI, HTTPException, Depends, status
 
-from .store import UserStore, User, get_db, user_from_db
+from .store import UserStore, User, get_db
 
 
 app = FastAPI(
@@ -13,9 +13,18 @@ app = FastAPI(
 )
 
 
+async def get_existing_user(db: UserStore, cpf: str) -> User:
+    user = await db.get_user(cpf)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist."
+        )
+    return user
+
+
 @app.get("/users", response_model=List[User])
 async def get_users(db: UserStore = Depends(get_db)):
-    return [user_from_db(item) for item in await db.get_all_users()]
+    return await db.get_all_users()
 
 
 error_message_for_duplicate_user = "Duplicate user. The CPF is already registered."
@@ -49,34 +58,23 @@ async def put_user(cpf: str, user: User, db: UserStore = Depends(get_db)):
             detail="CPF in the path does not match CPF in body.",
         )
 
-    if not await db.get_user(cpf):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist."
-        )
+    # will throw exception if user does not exist
+    await get_existing_user(db, cpf)
     await db.update_user(cpf, user)
     return user
 
 
 @app.get("/users/{cpf}", response_model=User)
 async def get_user(cpf: str, db: UserStore = Depends(get_db)):
-    u = await db.get_user(cpf)
-    if not u:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist."
-        )
-    return user_from_db(u)
+    return await get_existing_user(db, cpf)
 
 
 @app.delete("/users/{cpf}", response_model=User)
 async def delete_user(cpf: str, db: UserStore = Depends(get_db)):
-    u = await db.get_user(cpf)
-    if not u:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist."
-        )
-    else:
-        await db.delete_user(cpf)
-    return user_from_db(u)
+    # will throw exception if user does not exist
+    user = await get_existing_user(db, cpf)
+    await db.delete_user(cpf)
+    return user
 
 
 if __name__ == "__main__":  # pragma: no cover
--- src/personapi/store.py
+++ src/personapi/store.py
@@ -4,6 +4,7 @@ import os
 from datetime import date, datetime
 
 from asyncio import sleep
+from typing import Dict, List, Union
 from bradocs4py import CPF
 from fastapi import Depends
 from pydantic import BaseModel, BaseSettings, EmailStr, validator
@@ -41,6 +42,15 @@ class User(BaseModel):
             # represent as date only to json
             datetime: lambda v: v.strftime("%Y-%m-%d"),
         }
+        schema_extra = {
+            "example": {
+                "firstName": "Mickey",
+                "lastName": "Mouse",
+                "cpf": "609.350.354-27",
+                "email": "mickey.mouse@disney.com",
+                "birthDate": "1928-11-18",
+            }
+        }
 
 
 class Settings(BaseSettings):
@@ -58,20 +68,6 @@ async def get_db(settings: Settings = Depends(get_settings)):
     return UserStore(settings.db_conn_str, settings.simulated_delay_seconds)
 
 
-def user_from_db(user_db_item):
-    """Transform a mongo document on a User class object.
-
-    A hack while I do not understand enough of fastapi and pydantic to do it better."
-    """
-    return User(
-        firstName=user_db_item["firstName"],
-        lastName=user_db_item["lastName"],
-        cpf=user_db_item["cpf"],
-        email=user_db_item["email"],
-        birthDate=user_db_item["birthDate"],
-    )
-
-
 # thread unsafe singleton from https://refactoring.guru/design-patterns/singleton/python
 class SingletonMeta(type):
     """
@@ -80,7 +76,7 @@ class SingletonMeta(type):
     metaclass because it is best suited for this purpose.
     """
 
-    _instances = {}
+    _instances: Dict[type, type] = {}
 
     def __call__(cls, *args, **kwargs):
         """
@@ -94,29 +90,39 @@ class SingletonMeta(type):
 
 
 class UserStore(metaclass=SingletonMeta):
-    def __init__(self, conn_string, simulated_delay_seconds=0):
+    def __init__(self, conn_string: str, simulated_delay_seconds=0):
         print("[PID %d] Connecting to %s" % (os.getpid(), conn_string))
         self.client = AsyncIOMotorClient(conn_string)
         self.db = self.client["people"]
         self.simulated_delay_seconds = simulated_delay_seconds
         print("[PID %d] New MongoDB connection opened." % os.getpid())
 
-    async def insert_user(self, user):
+    async def insert_user(self, user: User) -> None:
+        "Inserts user into the database."
         await self.db.users.insert_one(dict(user))
 
-    async def update_user(self, cpf, user):
-        await self.db.users.replace_one({"cpf": cpf}, dict(user))
+    async def update_user(self, cpf: str, user: User) -> None:
+        "Updates user with specified cpf."
+        await self.db.users.replace_one({"cpf": cpf}, user.dict())
 
-    async def delete_user(self, cpf):
+    async def delete_user(self, cpf: str) -> None:
+        "Deletes user with specified cpf."
         await self.db.users.delete_one({"cpf": cpf})
 
-    async def get_user(self, cpf):
+    async def get_user(self, cpf: str) -> Union[User, None]:
+        "Get user with specified cpf. Returns None if not found."
         if self.simulated_delay_seconds > 0:
-            await sleep(self.simulated_delay_seconds)
-        return await self.db.users.find_one({"cpf": cpf})
+            await sleep(self.simulated_delay_seconds)  # pragma: nocover
+        user = await self.db.users.find_one({"cpf": cpf})
+        if user:
+            return User(**user)
+        else:
+            return None
 
-    async def get_all_users(self):
+    async def get_all_users(self) -> List[User]:
+        "Get a list of all users."
         # this would not be wise on a huge db (loads all db in memory at once),
         # but will suffice here
+        # TODO: fix the hard-coded max here
         users = await self.db.users.find().to_list(MAX_USERS)
-        return users
+        return [User(**u) for u in users]
